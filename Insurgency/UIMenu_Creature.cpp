93d58f793b15929d4970b56811b5f894ecd2cce0
Elliot Hatch <elliot.hatch@gmail.com>
2012-11-30 18:25:22 -0700

    Initial Creature Menu

--- Insurgency/UIMenu_Creature.cpp
+++ Insurgency/UIMenu_Creature.cpp
@@ -3,7 +3,18 @@
 
 
 UIMenu_Creature::UIMenu_Creature(void)
+	:UIMenu(),
+	m_inventoryMenuList(),
+	m_inventoryMenuOption("Inventory", &m_inventoryMenuList)
 {
+	UIMenuList trunk;
+	
+	m_inventoryMenuOption.m_helpText = "Items in the creature's backpack";
+
+	trunk.m_options.push_back(&m_inventoryMenuOption);
+
+	setTrunk(&trunk);
+	//m_helpText = currentMenuList
 }
 
 
--- Insurgency/UIMenu_Creature.h
+++ Insurgency/UIMenu_Creature.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "UIMenu.h"
+#include "UIMenuList_Inventory.h"
 class UIMenu_Creature :
 	public UIMenu
 {
 public:
 	UIMenu_Creature(void);
 	~UIMenu_Creature(void);
+private:
+	UIMenuList_Inventory m_inventoryMenuList;
+	UIMenuOption m_inventoryMenuOption;
 };
 
